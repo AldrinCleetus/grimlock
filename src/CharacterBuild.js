39a2d26fb0586d4269c3f3490564b685505ac7ba
Aldrin <cleetusaldrin30@gmail.com>
2021-12-08 07:36:09 +0530

    basic character portrait done

--- src/CharacterBuild.js
+++ src/CharacterBuild.js
@@ -1,5 +1,3 @@
-
-
 const CharacterBuild = ()=>{
 
     return(
@@ -8,6 +6,28 @@ const CharacterBuild = ()=>{
                     <div>
                     <h1 className="">Character Build</h1>
                     </div>
+
+                    <div className="character-build container p-2">
+                        <div className="m-1">
+                        <input class="input is-rounded" type="text" placeholder="Enter Build Name: "/>
+                        </div>
+
+                        
+
+                        <div className="columns p-2">
+
+                            <div className="character-splash p-1">
+                                <img className="character-splash-img" src="https://api.genshin.dev/characters/bennett/gacha-splash" alt="error" />
+                            </div>
+
+
+                        </div>
+
+
+
+                    </div>
+
+                   
             </div>
         </div>
     )
--- src/Testing.js
+++ src/Testing.js
@@ -8,10 +8,23 @@ const Testing = () => {
 
                     <div className="columns mt-3 is-centered">
                         <div className="column is-1">
-                            <div class="card charactericon has-text-black characterSelector has-background-orange">
+                            <div class="card charactericon has-text-black characterSelector has-background-purple">
                                 <div class="card-image">
                                     <figure class="image">
-                                    <img src="https://api.genshin.dev/characters/yoimiya/icon-big" alt="test" />
+                                    <img src="https://api.genshin.dev/characters/bennett/icon" alt="test" />
+                                    </figure>
+                                </div>
+                                <div class=" is-text-overflow has-background-link rounded-corner">
+                                    <p className="is-size-7">Bennett</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="column is-1">
+                            <div class="card charactericon has-text-black characterSelector has-background-gold">
+                                <div class="card-image">
+                                    <figure class="image">
+                                    <img src="https://api.genshin.dev/characters/yoimiya/icon" alt="test" />
                                     </figure>
                                 </div>
                                 <div class=" is-text-overflow has-background-link rounded-corner">
@@ -24,11 +37,11 @@ const Testing = () => {
                             <div class="card charactericon has-text-black characterSelector has-background-gold">
                                 <div class="card-image">
                                     <figure class="image">
-                                    <img src="https://api.genshin.dev/characters/yoimiya/icon-big" alt="test" />
+                                    <img src="https://api.genshin.dev/characters/tartaglia/icon" alt="404" />
                                     </figure>
                                 </div>
                                 <div class=" is-text-overflow has-background-link rounded-corner">
-                                    <p className="is-size-7">Yoimia</p>
+                                    <p className="is-size-7">Childe</p>
                                 </div>
                             </div>
                         </div>
