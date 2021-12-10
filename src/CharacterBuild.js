234d56ee87970bf753315919fd4cc80b8ab27fce
Aldrin <cleetusaldrin30@gmail.com>
2021-12-10 06:27:29 +0530

    updated character build

--- src/CharacterBuild.js
+++ src/CharacterBuild.js
@@ -7,14 +7,21 @@ const CharacterBuild = ()=>{
                     <h1 className="">Character Build</h1>
                     </div>
 
+
+                    
+
                     <div className="character-build container p-2">
-                        <div className="m-1">
+                        <div className="mt-2 ">
                         <input class="input is-rounded" type="text" placeholder="Enter Build Name: "/>
                         </div>
 
+                        <div className="horizontal-bar"></div>
+
                         
 
-                        <div className="columns p-2 ">
+                        
+
+                        <div className="columns p-2">
 
                             <div className="character-splash p-1 column is-5">
                                 <img className="character-splash-img" src="https://api.genshin.dev/characters/bennett/gacha-splash" alt="error" />
@@ -22,7 +29,7 @@ const CharacterBuild = ()=>{
 
                             <div className="column is-2 ">
                                         <h6 className="is-size-4">Weapons</h6>
-                                        <div class="card charactericon has-text-black characterSelector has-background-grey my-3 mx-auto is-size-2">
+                                        <div class="card charactericon has-text-black characterSelector has-background-grey my-4 mx-auto is-size-2">
                                             <div class="card-image">
                                                 <figure class="image">
                                                 <img src="images/48.png" alt="test" />
@@ -33,7 +40,7 @@ const CharacterBuild = ()=>{
                                             </div>
                                         </div>
 
-                                        <div class="card charactericon has-text-black characterSelector has-background-grey my-3 mx-auto is-size-2">
+                                        <div class="card charactericon has-text-black characterSelector has-background-grey my-4 mx-auto is-size-2">
                                             <div class="card-image">
                                                 <figure class="image">
                                                 <img src="images/48.png" alt="test" />
@@ -44,7 +51,7 @@ const CharacterBuild = ()=>{
                                             </div>
                                         </div>
 
-                                        <div class="card charactericon has-text-black characterSelector has-background-grey my-3 mx-auto is-size-2">
+                                        <div class="card charactericon has-text-black characterSelector has-background-grey my-4 mx-auto is-size-2">
                                             <div class="card-image">
                                                 <figure class="image">
                                                 <img src="images/48.png" alt="test" />
@@ -56,7 +63,55 @@ const CharacterBuild = ()=>{
                                         </div>
 
                                         
-                                    </div>
+
+                                        
+
+                                        
+                            </div>
+                            
+
+                            <div className="column px-0 ">
+                                <div className="bar">
+
+                                </div>
+                            </div>
+
+
+                            <div className="column is-5">
+                                <h6 className="is-size-4 ">Artifacts</h6>
+                                <div className="columns">
+                                    <div class="card charactericon has-text-black characterSelector has-background-grey my-4 mx-auto is-size-2">
+                                                <div class="card-image">
+                                                    <figure class="image">
+                                                    <img src="images/goblet.png" alt="test" />
+                                                    </figure>
+                                                </div>
+                                                <div class=" is-text-overflow has-background-link rounded-corner">
+                                                    <p className="is-size-7">Select</p>
+                                                </div>
+                                            </div>
+                                            <div class="card charactericon has-text-black characterSelector has-background-grey my-4 mx-auto is-size-2">
+                                                <div class="card-image">
+                                                    <figure class="image">
+                                                    <img src="images/timepiece.png" alt="test" />
+                                                    </figure>
+                                                </div>
+                                                <div class=" is-text-overflow has-background-link rounded-corner">
+                                                    <p className="is-size-7">Select</p>
+                                                </div>
+                                            </div>
+                                            <div class="card charactericon has-text-black characterSelector has-background-grey my-4 mx-auto is-size-2">
+                                                <div class="card-image">
+                                                    <figure class="image">
+                                                    <img src="images/circlet.png" alt="test" />
+                                                    </figure>
+                                                </div>
+                                                <div class=" is-text-overflow has-background-link rounded-corner">
+                                                    <p className="is-size-7">Select</p>
+                                                </div>
+                                            </div>
+                                </div>
+                            </div>
                                 
 
 
