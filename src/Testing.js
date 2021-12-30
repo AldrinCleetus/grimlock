0e99e22f670871f3352d95487591d11f2f9a654d
Aldrin <cleetusaldrin30@gmail.com>
2021-12-30 22:39:07 +0530

    Added character selecter modal

--- src/Testing.js
+++ src/Testing.js
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 import GetCharacters from './api/GetCharacters'
+
 
 const Testing = () => {
 
 
+    const [characterSelecter,setCharacterSelecter] = useState(false)
+
+    const characterSelecterClicked = ()=>{
+        setCharacterSelecter(!characterSelecter)
+    }
 
     return(
         <div className="column max-height has-background-primary has-text-info">
@@ -11,11 +18,20 @@ const Testing = () => {
                     <h1 className="">Characters!</h1>
                     </div>
 
-                    <div className="columns mt-3 is-centered is-multiline">
-                        
-                        <GetCharacters></GetCharacters>
-
+                    <div className={'has-background-black ' + (characterSelecter ? 'is-active': '')} onClick={characterSelecterClicked}>
+                        Hello
                     </div>
+
+                    <div className={'has-background-black modal' + (characterSelecter ? 'is-active': '')} >
+                        <div className="modal-background"></div>
+                        <div className="modal-content">
+                            <div className="columns m-3 is-centered is-multiline " >
+                                <GetCharacters></GetCharacters>
+                            </div>
+                        </div>
+                        <button className="modal-close is-large" aria-label="close" onClick={characterSelecterClicked}></button>
+                    </div>
+
             </div>
         </div>
     )
