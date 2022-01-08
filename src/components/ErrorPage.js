ebe78267513832faa22298728baa238620374a22
Aldrin <cleetusaldrin30@gmail.com>
2022-01-08 20:06:13 +0530

    Added error page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,8 +1,17 @@
 const ErrorPage = () => {
-    return ( 
-        <div className="text-centered">
-            <h2>Error 404</h2>
-        </div>
+    return (
+        <>
+            
+            <div className="section columns has-text-info is-large has-text-centered heightfull is-size-1 p-0 m-auto">
+               
+                <div className="column is-full  p-0 m-auto">
+                <img src="../images/144.png" alt="404" className="errorPage"/>
+                <h2>Error 404</h2>
+                <h3>Paimon not found</h3>
+                    
+                </div>
+            </div> 
+        </> 
      );
 }
  
